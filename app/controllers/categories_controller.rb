e1e7ee818f09950cdc85fa39293811afd1b222ee
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-12 15:43:23 +0600

    Add CRUD ability to categories
    1. Add new actions to categories controller;
    2. Edit views;
    3. Fix all related with that staff in themes;
    4. Edit routes;
    5. Edit some styles;
    6. Add some auth to pages (content availiable only for current_user).

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,61 @@
 class CategoriesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :find_category, only: [:edit, :update, :destroy]
+
+  # /categories GET
   def index
     @categories = Category.all
+  end
+
+  # /categories/new GET
+  def new
+    @category = current_user.categories.new
+  end
+
+  # # /categories/:id GET
+  # def show; end
+
+  # /categories/:id/edit GET
+  def edit; end
+
+  # /categories POST
+  def create
+    @category = current_user.categories.create(category_params)
+    if @category.errors.empty?
+      redirect_to categories_path, success: "Category #{@category.name} is successfully created!"
+    else
+      flash.now[:error] = 'Oops, you made some mistakes below.'
+      render :new
+    end
+  end
+
+  # /categories/:id PUT, PATCH
+  def update
+    @category.update_attributes(category_params)
+    if @category.errors.empty?
+      redirect_to category_themes_path(@category), success: "Category #{@category.name} is successfully updated!"
+    else
+      flash.now[:error] = 'Oops, you made some mistakes below.'
+      render :edit
+    end
+  end
+
+  # /categories/:id DELETE
+  def destroy
+    @category.destroy
+    flash[:success] = 'This category is successfully deleted!'
+    redirect_to categories_path
+  end
+
+  private
+
+  def find_category
+    @category = Category.where(id: params[:id]).first
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
