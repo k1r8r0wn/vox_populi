6c8c3f5e404d173ff223a62c0436b78b5b7a8bd3
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-12 17:47:40 +0600

    Create factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    username 'Username'
+    sequence(:email) { |i| "email#{i}@mail.com" }
+    password 'password'
+    password_confirmation 'password'
   end
 end
