e5c6f41aa17a341c2da8c1be52f8cfab15feeb77
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-13 23:02:46 +0600

    :new, :create actions & separate ones work

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,18 +1,68 @@
 class ThemesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create, :new_separate, :create_separate]
   before_action :find_category, only: [:index]
+  before_action :find_category_current_user, only: [:new, :create]
+  before_action :find_theme, only: [:show]
 
+  # /categories/:category_id/themes GET
   def index
     @themes = Theme.where(category_id: params[:category_id])
   end
 
-  def new; end
+  # /themes/new GET
+  def new
+    @theme = @category.themes.new
+  end
+
+  # /themes/new GET
+  def new_separate
+    @theme = Theme.new
+    @categories = Category.all
+  end
+
+  # /themes/create POST
+  def create_separate
+    @theme = current_user.themes.new(theme_params)
+    if @theme.save
+      redirect_to category_themes_path(@theme.category), success: "Theme #{@theme.title} is successfully created!"
+    else
+      flash.now[:error] = 'Oops, you made some mistakes below.'
+      render :new_separate
+    end
+  end
+
+  # /categories/:category_id/themes/:id GET
+  def show; end
+
+  # /themes POST
+  def create
+    @theme = @category.themes.new(theme_params)
+    if @theme.save
+      redirect_to category_themes_path, success: "Theme #{@theme.title} is successfully created!"
+    else
+      flash.now[:error] = 'Oops, you made some mistakes below.'
+      render :new
+    end
+  end
 
   private
 
+  def find_theme
+    @theme = Theme.where(category_id: params[:category_id], id: params[:id]).first
+    render_404 unless @theme
+  end
+
   def find_category
     @category = Category.find(params[:category_id])
+  end
+
+  def find_category_current_user
+    @category = current_user.categories.find(params[:category_id])
+  end
+
+  def theme_params
+    params.require(:theme).permit(:title, :content, :category_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :categories, only: [:index, :new, :create, :edit, :update, :destroy] do
-    resources :themes, only: [:index]
+    resources :themes, only: [:index, :show, :new, :create]
   end
 
-  resources :themes, only: [:new, :create]
+  get 'themes/new' => 'themes#new_separate', as: :new_separate
+  post 'themes/create' => 'themes#create_separate', as: :create_separate
 
   root 'pages#home'
 
