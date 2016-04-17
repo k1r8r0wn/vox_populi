3780ca55b5dd5e5d7df7f573b2a9bdb60998f42b
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-17 18:01:36 +0600

    Fix themes factories

--- spec/factories/themes.rb
+++ spec/factories/themes.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :theme do
     sequence(:title) { |i| "Title#{i}" }
-    content 'Content'
+    sequence(:content) { |i| "Content#{i}" }
     user
     category
   end
