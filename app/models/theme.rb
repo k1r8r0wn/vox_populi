e0b8447d1344593dfc5f0c4921af798046851c69
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-18 11:18:02 +0600

    Update other models

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,6 +2,7 @@ class Theme < ActiveRecord::Base
 
   belongs_to :category
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, :content, presence: true
   validates :title, uniqueness: { case_sensitive: false }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   has_many :categories, dependent: :destroy
   has_many :themes, dependent: :destroy
+  has_many :comments
 
   validates :username, presence: true
   validates :username, uniqueness: true
