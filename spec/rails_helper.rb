42e35aea24c8011f5932f8fd41ef74b3dca90570
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-24 19:45:33 +0600

    Add capybara-webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,9 +8,11 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
 require 'capybara/email/rspec'
+require 'capybara/webkit/matchers'
 require 'simplecov'
 SimpleCov.start
 require 'devise'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -36,6 +38,9 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include AuthenticationHelpers::Feature, type: :feature
+
+  Capybara.javascript_driver = :webkit
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
