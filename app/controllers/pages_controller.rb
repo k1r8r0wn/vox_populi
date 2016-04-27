998f18e7bd0524b45adbdb660830606d5947fe33
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-27 21:10:57 +0600

    Add statistics action to pages controller, add view, edit routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
 
-  layout 'home'
+  layout 'home', only: [:home]
 
   def home; end
+
+  def statistics
+    @themes = []
+
+    category = Category.eager_load(:themes)
+    category.each do |category|
+      @themes << [ category.name, category.themes.size ]
+    end
+  end
 
 end
