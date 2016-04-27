46b1995324597269cb3b6031419a6505c6e3aae9
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-27 14:30:13 +0600

    Add theme policy spec

---
+++ spec/policies/theme_policy_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe ThemePolicy do
+
+  let(:user) { create(:user) }
+
+  subject { described_class }
+
+  permissions :new?, :new_separate?, :create?, :create_separate? do
+    it 'grants access for every user'  do
+      expect(subject).to permit(create(:user))
+      expect(subject).to permit(create(:admin))
+      expect(subject).to permit(create(:moderator))
+    end
+  end
+
+  permissions :edit?, :update? do
+    it 'grants access only for admin, moderator' do
+      expect(subject).to permit(create(:admin))
+      expect(subject).to permit(create(:moderator))
+    end
+
+    it 'grant access if user is author of the theme' do
+      expect(subject).to permit(user, create(:theme, user: user))
+    end
+
+    it 'denies access if user is not author of the theme' do
+      expect(subject).not_to permit(User.new, create(:theme, user: user))
+    end
+  end
+
+  permissions :destroy? do
+    it 'grants access only for admin or moderator' do
+      expect(subject).to permit(create(:admin))
+      expect(subject).to permit(create(:moderator))
+    end
+
+    it 'denies access if user is author of the theme' do
+      expect(subject).not_to permit(user, create(:theme, user: user))
+    end
+
+    it 'denies access if user is not author of the theme' do
+      expect(subject).not_to permit(User.new, create(:theme, user: user))
+    end
+  end
+
+end
